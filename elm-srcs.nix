14f0e5d2e3bf128e2b446066459cb0586ec76f2d
Kristoffer Dalby <kristoffer@dalby.cc>
2026-06-13 22:41:53 +0200

    ci: named flake-check jobs (build, formatting) with treefmt
    build = pure elm make; PDF render + Pages deploy stay in workflow.

---
+++ elm-srcs.nix
@@ -0,0 +1,67 @@
+{
+
+  "NoRedInk/elm-json-decode-pipeline" = {
+    sha256 = "0y25xn0yx1q2xlg1yx1i0hg4xq1yxx6yfa99g272z8162si75hnl";
+    version = "1.0.0";
+  };
+
+  "elm-community/list-extra" = {
+    sha256 = "0gsy04gw9nilb24fnc4rkxj255f904yjg8z5qcf1iin0c9rgd7c4";
+    version = "8.4.0";
+  };
+
+  "elm/browser" = {
+    sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
+    version = "1.0.2";
+  };
+
+  "elm/core" = {
+    sha256 = "19w0iisdd66ywjayyga4kv2p1v9rxzqjaxhckp8ni6n8i0fb2dvf";
+    version = "1.0.5";
+  };
+
+  "elm/json" = {
+    sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
+    version = "1.1.3";
+  };
+
+  "rtfeldman/elm-css" = {
+    sha256 = "189pf91bdpyz4qyilbzjh2mj3x46j7abb9asnkiv1chzvhcamrn5";
+    version = "16.1.1";
+  };
+
+  "elm/html" = {
+    sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
+    version = "1.0.0";
+  };
+
+  "elm/time" = {
+    sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+    version = "1.0.0";
+  };
+
+  "elm/url" = {
+    sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
+    version = "1.0.0";
+  };
+
+  "elm/virtual-dom" = {
+    sha256 = "0q1v5gi4g336bzz1lgwpn5b1639lrn63d8y6k6pimcyismp2i1yg";
+    version = "1.0.2";
+  };
+
+  "rtfeldman/elm-hex" = {
+    sha256 = "1y0aa16asvwdqmgbskh5iba6psp43lkcjjw9mgzj3gsrg33lp00d";
+    version = "1.0.0";
+  };
+
+  "elm-explorations/test" = {
+    sha256 = "1fsd7bajm7qa93r5pn3mdafqh3blpzya601jbs9l238p0hmvh576";
+    version = "1.2.2";
+  };
+
+  "elm/random" = {
+    sha256 = "138n2455wdjwa657w6sjq18wx2r0k60ibpc4frhbqr50sncxrfdl";
+    version = "1.0.0";
+  };
+}
--- flake.nix
+++ flake.nix
@@ -2,58 +2,91 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }: let
-    infrastructureVersion =
-      if (self ? shortRev)
-      then self.shortRev
-      else "dev";
-  in
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , treefmt-nix
+    , ...
+    }:
     {
-      overlay = final: prev: let
-        pkgs = nixpkgs.legacyPackages.${prev.system};
-      in rec {};
+      overlay = final: prev:
+        let
+          pkgs = nixpkgs.legacyPackages.${prev.system};
+        in
+        rec { };
     }
     // flake-utils.lib.eachDefaultSystem
-    (system: let
-      pkgs = import nixpkgs {
-        overlays = [self.overlay];
-        inherit system;
-      };
-      devDeps = with pkgs;
-        [
-          nodejs
-          nodePackages.yarn
-          # x11-xkb-utils
-          # x11-apps
-          # clang
-          # libdbus-1-dev
-          # # libgtk2.0-dev
-          # libnotify-dev
-          # libgconf2-dev
-          # libasound2-dev
-          # libcap-dev
-          # libcups2-dev
-          # libxtst-dev
-          # libxss1
-          # libnss3-dev
-        ]
-        ++ lib.optionals pkgs.stdenv.isLinux [
-          xvfb-run
-        ];
-    in rec {
-      # `nix develop`
-      devShell = pkgs.mkShell {
-        buildInputs = devDeps;
-        shellHook = ''
-          export DISPLAY=:9.0
-        '';
-      };
-    });
+      (system:
+      let
+        pkgs = import nixpkgs {
+          overlays = [ self.overlay ];
+          inherit system;
+        };
+
+        devDeps = with pkgs;
+          [
+            nodejs
+            yarn
+          ]
+          ++ lib.optionals pkgs.stdenv.isLinux [
+            xvfb-run
+          ];
+
+        # treefmt: single source of truth for formatting (elm, nix, web).
+        treefmt = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.elm-format.enable = true;
+          programs.nixpkgs-fmt.enable = true;
+          programs.prettier.enable = true;
+        };
+
+        # Pure Elm compilation of the resume source. The full parcel bundle +
+        # electron PDF render + Pages deploy need node_modules / a headless
+        # display and stay in the workflow; this check guards the Elm source.
+        build = pkgs.stdenv.mkDerivation {
+          pname = "resume";
+          version = "1.0.0";
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.elmPackages.elm ];
+
+          configurePhase = pkgs.elmPackages.fetchElmDeps {
+            elmPackages = import ./elm-srcs.nix;
+            elmVersion = "0.19.1";
+            registryDat = ./registry.dat;
+          };
+
+          buildPhase = ''
+            elm make src/Main.elm --output=resume.html
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp resume.html $out/
+          '';
+        };
+      in
+      {
+        packages.default = build;
+
+        checks = {
+          inherit build;
+          formatting = treefmt.config.build.check self;
+        };
+
+        formatter = treefmt.config.build.wrapper;
+
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          buildInputs = devDeps;
+          shellHook = ''
+            export DISPLAY=:9.0
+          '';
+        };
+      });
 }
